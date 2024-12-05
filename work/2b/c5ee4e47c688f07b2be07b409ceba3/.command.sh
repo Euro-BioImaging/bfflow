8df1817f286adf4c9d6e69a86cb233bda5692973
bugraoezdemir <bugraa.ozdemir@gmail.com>
2024-12-05 12:35:16 +0100

    slurm support

---
+++ work/2b/c5ee4e47c688f07b2be07b409ceba3/.command.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e # Exit if a tool returns a non-zero status/exit code
+set -u # Treat unset variables and parameters as an error
+set -o pipefail # Returns the status of the last command to exit with a non-zero status or zero if all successfully execute
+set -C # No clobber - prevent output redirection from overwriting files.
+
+bfconvert         filament.tif         i1.ome.tiff         -z 5     -compression LZW
